e2160dc363d6fe79a3efbe1e3afc4f8df3420f20
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-15 11:07:32 -0300

    feat: add specifications repository

---
+++ src/modules/cars/repositories/SpecificationsRepository.repository.ts
@@ -0,0 +1,26 @@
+import { Specification } from '../model/Specification';
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from './interfaces/ISpecificationsRepository.repository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[] = [];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(item => item.name === name);
+    return specification;
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+    Object.assign(specification, { name, description, created_at: new Date() });
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationsRepository };
