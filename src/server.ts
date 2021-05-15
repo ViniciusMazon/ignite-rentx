98476abadbaf1fac52e1ebfbb6b81077700cfeef
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-15 12:48:06 -0300

    feat: unify routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 app.use(express.json());
 
-app.use('/v1/categories', categoriesRoutes);
-app.use('/v1/specifications', specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => {
   console.clear();
