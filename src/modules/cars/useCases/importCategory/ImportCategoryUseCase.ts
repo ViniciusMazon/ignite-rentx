09c81dfa289a91f2b3b87166f4bc8e34b07960ed
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-16 09:25:54 -0300

    feat: add remove file on ImportCategory use case

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -23,6 +23,7 @@ class ImportCategoryUseCase {
           categories.push({ name, description });
         })
         .on('end', () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on('error', err => {
