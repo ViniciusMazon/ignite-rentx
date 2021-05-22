ce874e7a629ef95e56a3af2bf08d14f19ff1f48e
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-22 19:16:51 -0300

    feat: add specifications migration

---
+++ src/database/migrations/1621721584517-CreateSpecifications.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateSpecifications1621721584517 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: 'specifications',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('specifications');
+  }
+}
