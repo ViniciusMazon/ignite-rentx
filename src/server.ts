0ac7331059629b0a3a66cfc43c52a37f29ba0974
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-22 18:43:02 -0300

    feat: add category injection

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 import './database';
 import { router } from './routes';
+import './shared/container';
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
