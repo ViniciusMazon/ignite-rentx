3e7a06ac61db0a4151eb932d2c6fbb6fd0bfbb3e
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-22 19:18:45 -0300

    feat: update specification entitiy

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,12 +1,19 @@
 import { v4 as uuid } from 'uuid';
+import { Entity, Column, PrimaryKey, CreateDateColumn } from 'typeorm';
 
+@Entity()
 class Specification {
+  @PrimaryKey()
   public id: string;
 
+  @Column()
   public name: string;
 
+  @Column()
   public description: string;
 
+  @CreateDateColumn()
+  // eslint-disable-next-line camelcase
   public created_at: Date;
 
   constructor() {
