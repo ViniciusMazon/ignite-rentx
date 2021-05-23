4e25abf92e1d837ce37a07c791793d2c4272cce6
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-23 11:32:50 -0300

    feat: add session use case

---
+++ src/modules/accounts/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,51 @@
+import { inject, injectable } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
+import { UsersRepository } from '../repositories/implementations/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: UsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const passwordMatch = compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const token = sign({}, 'd41d8cd98f00b204e9800998ecf8427e', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      user: { name: user.name, email: user.email },
+      token,
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
