3a3e062bd5be2afefa309e4592ca5f161cba996d
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-29 10:49:28 -0300

    feat: add cars repository and DTO

---
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
+import { Car } from '../infra/typeorm/entities/Car';
+
+interface ICarsRepository {
+  create(data: ICreateCarDTO): Promise<Car>;
+  findByLicensePlate(license_plate: string): Promise<Car>;
+}
+
+export { ICarsRepository };
---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '../ICarsRepository';
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<Car> {
+    const car = new Car();
+    Object.assign(car, {
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    this.cars.push(car);
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.cars.find(car => car.license_plate === license_plate);
+  }
+}
+
+export { CarsRepositoryInMemory };
