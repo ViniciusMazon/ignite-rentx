02df61f91018089518cccdcafeece8f033be6b14
Vinicius Mazon <vnpmazon@gmail.com>
2021-06-26 20:48:47 -0300

    feat: add IRentalsRepository

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,11 @@
+import { ICreateRentalDTO } from '../dtos/ICreateRentalDTO';
+import { Rental } from '../infra/typeorm/entities/Rental';
+
+/* eslint-disable camelcase */
+interface IRentalsRepository {
+  findOpenRentalByCarId(car_id: string): Promise<Rental>;
+  findOpenRentalByUserId(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
+}
+
+export { IRentalsRepository };
