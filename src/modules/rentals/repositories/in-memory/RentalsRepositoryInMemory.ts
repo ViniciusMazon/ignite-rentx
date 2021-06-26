dac4b3f616444202f1042aac72f372bdbdf09686
Vinicius Mazon <vnpmazon@gmail.com>
2021-06-26 20:49:00 -0300

    feat: add RentalsRepositoryInMemory

---
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '../IRentalsRepository';
+
+class RentalsRepositoryInMemory implements IRentalsRepository {
+  rentals: Rental[] = [];
+
+  async create({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+    Object.assign(rental, {
+      car_id,
+      user_id,
+      expected_return_date,
+      start_date: new Date(),
+    });
+    this.rentals.push(rental);
+    return rental;
+  }
+
+  async findOpenRentalByCarId(car_id: string): Promise<Rental> {
+    return this.rentals.find(
+      rental => rental.car_id === car_id && !rental.end_date,
+    );
+  }
+
+  async findOpenRentalByUserId(user_id: string): Promise<Rental> {
+    return this.rentals.find(
+      rental => rental.user_id === user_id && !rental.end_date,
+    );
+  }
+}
+
+export { RentalsRepositoryInMemory };
