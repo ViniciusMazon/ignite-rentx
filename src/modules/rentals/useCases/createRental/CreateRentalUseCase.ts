3ae4a8b4ecb7a0216857b813f91d6a4cb5fc72ed
Vinicius Mazon <vnpmazon@gmail.com>
2021-06-26 20:49:17 -0300

    feat: add create rental use case

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,43 @@
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { AppError } from '@shared/errors/AppError';
+
+/* eslint-disable camelcase */
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCarId(
+      car_id,
+    );
+    if (carUnavailable) {
+      throw new AppError('Car is unavailable');
+    }
+
+    const rentalOpenToUser =
+      await this.rentalsRepository.findOpenRentalByUserId(user_id);
+    if (rentalOpenToUser) {
+      throw new AppError("There's a rental in progress for user");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
