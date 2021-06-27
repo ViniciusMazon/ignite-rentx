cd816dbc385eb5d3bc1c58f6c9a781aaf31a1d33
Vinicius Mazon <vnpmazon@gmail.com>
2021-06-27 09:00:13 -0300

    chore: divide app and server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,32 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
-import swaggerFile from '../../../swagger.json';
-import { router } from './routes';
-import '@shared/container';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(router);
-
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error ${err.message}`,
-    });
-  },
-);
+import { app } from './app';
 
 app.listen(3333, () => {
   console.clear();
