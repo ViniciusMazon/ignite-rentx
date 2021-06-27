0db3f6d2afcfcc937f34912db928d90265b99c75
Vinicius Mazon <vnpmazon@gmail.com>
2021-06-27 09:41:37 -0300

    chore: update connection to use test database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,7 +4,11 @@ export default async (host = 'database-pg'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rentx_test'
+          : defaultOptions.database,
     }),
   );
 };
