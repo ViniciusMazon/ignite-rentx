ba5c946b117e77de31d32df0756fea13e2c58503
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-29 16:31:43 -0300

    feat: add mail provider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,5 @@
 import { container } from 'tsyringe';
-import '@shared/container/providers/date';
+import '@shared/container/providers';
 
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '@modules/cars/infra/repositories/CategoriesRepository';
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
 import { rentalsRoutes } from './rentals.routes';
+import { passwordRoutes } from './password.routes';
 
 const router = Router();
 
@@ -13,6 +15,7 @@ router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
 router.use('/cars', carsRoutes);
 router.use('/rentals', rentalsRoutes);
+router.use('/password', passwordRoutes);
 router.use(authenticateRoutes);
 
 export { router };
