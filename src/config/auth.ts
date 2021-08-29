a4cf0066df27f738f055150a568a3e89a91a12ac
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-29 11:01:01 -0300

    chore: add token config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'd41d8cd98f00b204e9800998ecf8427e',
+  expires_in_token: '15m',
+  secret_refresh_token: '4f0984270b4e07c54eefd4924a7a0e93',
+  expires_in_token_refresh_token: '30d',
+  expires_days_refresh_token: 30,
+};
