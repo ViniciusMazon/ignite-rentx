df8e44038908929eb795d26320da1b2d35c19e25
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-29 09:54:37 -0300

    feat: add repository

---
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -0,0 +1,13 @@
+
+import { ICreateUserTokenDTO } from '../dto/ICreateUserTokenDTO';
+import { UserTokens } from '../infra/typeorm/entities/UserTokens';
+
+interface IUsersTokensRepository {
+  create({
+    user_id,
+    expires_date,
+    refresh_token,
+  }: ICreateUserTokenDTO): Promise<UserTokens>;
+}
+
+export { IUsersTokensRepository };
